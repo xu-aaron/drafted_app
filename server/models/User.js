91c75835e111308aad17ee5b58979182b365615a
xu-aaron <aaronxu08@gmail.com>
2023-04-18 00:15:46 -0400

    Initial commit

---
+++ server/models/User.js
@@ -0,0 +1,49 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      min: 2,
+      max: 50,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      min: 2,
+      max: 50,
+    },
+    username: {
+      type: String,
+      required: true,
+      max: 50,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      min: 5,
+    },
+    picturePath: {
+      type: String,
+      default: "",
+    },
+    friends: {
+      type: Array,
+      default: [],
+    },
+    dob: String,
+    height: String,
+    weight: String,
+    handedness: String,
+    sport: String,
+    position: String,
+    team: String,
+    hometown: String,
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", UserSchema);
+export default User;
